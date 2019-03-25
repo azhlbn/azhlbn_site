673c8a53736b7fd87bbc355b687584e068adfa7b
Alelsey <azhlbn@gmail.com>
2019-03-25 19:21:50 +0300

    comment

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', main, name='main'),
+    path('fonts', fonts, name='fonts')
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main/main.html')
+
+
+def fonts(request):
+    return render(request, 'main/fonts.html')
+
